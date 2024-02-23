1439e7e424afeae088daeffff3121a7f331dd28b
himabindutellakula <tellakulahimabindu26@gmail.com>
2024-02-22 19:57:32 -0500

    minor fixes

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -5,9 +5,9 @@ import { FaEllipsisV } from "react-icons/fa";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 function Modules() {
     return (
-        <div>
-            <div className="wd-modules-buttons row">
-                <button className="btn btn-outline-secondary me-2 col-auto">Collapse All</button>
+        <div className="container">
+            <div className="wd-modules-buttons row ms-auto">
+                <button className="btn btn-outline-secondary me-2 col-auto ms-auto">Collapse All</button>
                 <button className="btn btn-outline-secondary me-2 col-auto">View Progress</button>
                 <div className="dropdown col-auto" style={{ marginRight:'7px',height: '30px', borderRadius: '5px' }}>
                     <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ padding: '5px 10px', height: '35px', borderRadius: '4px'}}>
@@ -19,7 +19,7 @@ function Modules() {
                     </ul>
                 </div>
                 <button className="btn btn-danger me-2 col-auto" style={{ backgroundColor: '#B22222', color: 'white' }}>+ Module</button>
-                <button className="btn btn-outline-secondary col-auto"><FaEllipsisV /></button>
+                <button className="btn btn-outline-secondary col-auto me-2"><FaEllipsisV /></button>
             </div>
             <hr style={{ border: '1px solid black' }} />
             <br />
