06ed6dfc1088fb5d3d490b5f28604e0e7c38446a
himabindutellakula <tellakulahimabindu26@gmail.com>
2024-03-17 19:31:32 -0400

    Lab 4 content

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -7,10 +7,22 @@ import Highlight from "./Highlight";
 import Add from "./add";
 import TodoItem from "./todos/TodoItem";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
+
 function Assignment3() {
+    const { todos } = useSelector((state: LabState) => state.todosReducer);
     return (
         <div>
             <h1>Assignment 3</h1>
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
+
             <ConditionalOutput />
             <Styles />
             <Classes />
@@ -21,8 +33,8 @@ function Assignment3() {
                 vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
             </Highlight>
             <Add a={3} b={4} />
-            <TodoItem/>
-            <TodoList/>
+            <TodoItem />
+            <TodoList />
         </div>
     );
 }
